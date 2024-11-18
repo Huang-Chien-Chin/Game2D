12e1d8a066d3a5cf1ff1ec7f7270f3074c695d91
hcc0507 <c0920434618@gmail.com>
2024-11-18 16:42:53 +0800

    2024/11/18

---
+++ app/src/main/java/tw/edu/pu/csim/s1120326/game2d/Background.kt
@@ -0,0 +1,14 @@
+package tw.edu.pu.csim.s1120326.game2d
+import kotlin.math.abs
+class Background(val screenW:Int) {
+        var x1 = 0
+        var x2 = screenW
+
+        fun Roll(){
+            x1 --
+            if (abs(x1) > screenW){
+                x1 = 0
+            }
+            x2 = x1 + screenW
+        }
+    }
